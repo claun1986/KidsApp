67fe17c8fe8218a65e28f2088b13aef486356c33
Sarah <claun@gmx.at>
2014-10-13 13:41:15 +0200

    2 screens

---
+++ app/src/main/java/com/example/kinderapp/screen1.java
@@ -0,0 +1,49 @@
+package com.example.kinderapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+/**
+ * Created by Sarah on 13.10.14.
+ */
+public class screen1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+}
+
---
+++ app/src/main/java/com/example/kinderapp/screen2.java
@@ -0,0 +1,7 @@
+package com.example.kinderapp;
+
+/**
+ * Created by Sarah on 13.10.14.
+ */
+public class screen2 {
+}
