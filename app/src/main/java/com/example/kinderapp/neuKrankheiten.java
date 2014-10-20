91cf8044af160b28d325a55f10e8f81b062a0f3a
Sarah <claun@gmx.at>
2014-10-20 12:13:35 +0200

    add medcontainer by click geht nicht

--- app/src/main/java/com/example/kinderapp/neuKrankheiten.java
+++ app/src/main/java/com/example/kinderapp/neuKrankheiten.java
@@ -2,11 +2,14 @@ package com.example.kinderapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Adapter;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 /**
  * Created by Sarah on 19.10.14.
@@ -25,10 +28,20 @@ public class neuKrankheiten extends Activity{
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        final LinearLayout ll_container = (LinearLayout) findViewById(R.id.newMedField);
 
         //NEUES MEDIKAMENTEN FELD HINZUFÜGEN
         Button add2 = (Button) findViewById(R.id.add2);
-        //add2.setOnClickListener();
+        add2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TextView tv1 = new TextView(getApplicationContext());
+                //tv1.setText("TEST");
+                LinearLayout ll = (LinearLayout) getLayoutInflater().inflate(R.layout.medcontainer, null);
+                ll_container.addView(ll);
+            }
+        });
 
 
 
