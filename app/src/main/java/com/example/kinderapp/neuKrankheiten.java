55e76d61f93d37e8d41a4f2f4d1a5a655d0a4918
Sarah <claun@gmx.at>
2014-10-21 10:02:11 +0200

    date picker dialog YEEEEAH

--- app/src/main/java/com/example/kinderapp/neuKrankheiten.java
+++ app/src/main/java/com/example/kinderapp/neuKrankheiten.java
@@ -1,6 +1,7 @@
 package com.example.kinderapp;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -11,6 +12,10 @@ import android.widget.Spinner;
 import android.widget.Adapter;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * Created by Sarah on 19.10.14.
@@ -22,6 +27,14 @@ public class neuKrankheiten extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.neukrankheiten);
+
+        Button datum = (Button) findViewById(R.id.datum);
+        datum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
 
         //SPINNER
         Spinner spinner = (Spinner) findViewById(R.id.spinner_dosierung);
@@ -44,10 +57,31 @@ public class neuKrankheiten extends Activity{
             }
         });
 
-        //CALENDER VIEW
-        CalendarView cw1 = (CalendarView) findViewById(R.id.cw1);
-        cw1.setDate(2014);
+//        //CALENDER VIEW
+//        CalendarView cw1 = (CalendarView) findViewById(R.id.cw1);
+//        cw1.setDate(2014);
+    }
+    private void showDatePickerDialog() {
+        // Datum von heute auslesen
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // DatePickerDialog erzeugen (hat 5 Parameter):
+        // Context (this), Listener, 3x initiales Datum
+        DatePickerDialog myFancyDatePicker = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Toast.makeText(getApplicationContext(), year + "-" + monthOfYear + "-" + dayOfMonth, Toast.LENGTH_LONG).show();
+                    }
+                }, mYear, mMonth, mDay);
+
+        // DatePickerDialog anschauen
+        myFancyDatePicker.show();
+    }
     }
 
 
-}
+
