b2c2873ccda4d771ab5cf82339970dd9bcf8f0af
Sarah <claun@gmx.at>
2014-10-27 21:13:41 +0100

    notification bar---geht a net....checks net

--- app/src/main/java/com/example/kidsapp/neuKrankheiten.java
+++ app/src/main/java/com/example/kidsapp/neuKrankheiten.java
@@ -3,6 +3,8 @@ package com.example.kidsapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +23,10 @@ public class neuKrankheiten extends Activity{
         setContentView(R.layout.neukrankheiten);
     }
 
+    public Notification.Builder.build() {
 
-
-
+    }
+    return NotificationManager.notify()
     }
 
 
